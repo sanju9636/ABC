3b144e44504a9726a53cc8402faf19f91aa77a14
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-05-10 23:06:31 +0530

    Update active.py

--- AnonXMusic/plugins/tools/active.py
+++ AnonXMusic/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 from unidecode import unidecode
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
