d6bf96e9977cd9209666f27ec24717b122eaa9ac
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-05-10 23:09:06 +0530

    Update ai.py

--- AnonXMusic/plugins/tools/ai.py
+++ AnonXMusic/plugins/tools/ai.py
@@ -1,7 +1,7 @@
 import random
 import time
 import requests
-from DAXXMUSIC import app
+from AnonXMusic import app
 from config import BOT_USERNAME
 
 from pyrogram.enums import ChatAction, ParseMode
