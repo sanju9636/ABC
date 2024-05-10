a565ffb1e709c71df25a523d3ab6c4be279150b6
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-05-10 23:08:11 +0530

    Update afk.py

--- AnonXMusic/plugins/tools/afk.py
+++ AnonXMusic/plugins/tools/afk.py
@@ -3,9 +3,9 @@ from config import BOT_USERNAME
 from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
-from DAXXMUSIC import app
-from DAXXMUSIC.mongo.readable_time import get_readable_time
-from DAXXMUSIC.mongo.afkdb import add_afk, is_afk, remove_afk
+from AnonXMusic import app
+from AnonXMusic.mongo.readable_time import get_readable_time
+from AnonXMusic.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
 
