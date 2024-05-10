c63b0ed87f807545ab3005b8d3e553228b1ed0e7
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-05-10 23:19:11 +0530

    Update quote.py

--- AnonXMusic/plugins/tools/quote.py
+++ AnonXMusic/plugins/tools/quote.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from DAXXMUSIC import app
+from AnonXMusic import app
 from httpx import AsyncClient, Timeout
 
 
