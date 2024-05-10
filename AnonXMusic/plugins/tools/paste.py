140d23df676dfc22c3aba7658c848aa9e7953a4d
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-05-10 23:17:52 +0530

    Update paste.py

--- AnonXMusic/plugins/tools/paste.py
+++ AnonXMusic/plugins/tools/paste.py
@@ -1,6 +1,6 @@
 from asyncio import get_running_loop, sleep, TimeoutError
 from functools import partial
-from DAXXMUSIC import app
+from AnonXMusic import app
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiohttp import ClientSession
